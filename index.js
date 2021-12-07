c471273e9cdb78872fedd6d11f5d18072f8fe068
milecubela <mile.cubela@gmail.com>
2021-12-07 18:51:37 +0100

    vise layera, stvaranje slika, metadata

--- index.js
+++ index.js
@@ -1,22 +1,73 @@
 const fs = require('fs')
+const myArgs = process.argv.slice(2)
 const {createCanvas, loadImage } = require('canvas')
-// Canvas 1000x1000 px
-const canvas = createCanvas(1000, 1000)
+const {layers, width, height} = require('./input/config')
+const canvas = createCanvas(width, height)
 // context
 const ctx = canvas.getContext('2d')
-const {layers, width, height} = require('./input/config')
+const edition = myArgs.length > 0 ? Number(myArgs[0]) : 1 // Broj razlicitih slika koje ce se generirati
+let metadata = []
+let attributes = []
+let hash = []
+let decodedHash = []
 
-
-const saveLayer = (_canvas) => {
-    fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"))
-    console.log("image created")
+// Pretvara canvas u png i sprema u /output/
+const saveLayer = (_canvas,_edition) => {
+    fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"))
 }
 
-const drawLayer = async () => {
-    const image = await loadImage("./slika.png")
-    ctx.drawImage(image, 0, 0, width, height)
-    console.log("this ran")
-    saveLayer(canvas)
+// Metadata za svaki edition
+const addMetaData = (_edition) => {
+    let dateTimeStamp = Date.now()
+    let tempMetaData = {
+        hash: hash.join(""),
+        decodedHash: decodedHash,
+        edition: _edition,
+        date: dateTimeStamp,
+        attributes: attributes
+    }
+    metadata.push(tempMetaData)
+    // Nakon spremanja metadata, resetiranje ostalih atributa za iduci edition
+    attributes = []
+    hash = []
+    decodedHash = []
 }
 
-drawLayer()
+const addAttributes = (_element, _layer) => {
+    let tempAttributes = {
+        id: _element.id,
+        layer: _layer.name,
+        name: _element.name,
+        rarity: _element.rarity
+    }
+    attributes.push(tempAttributes)
+    hash.push(_layer.id)
+    hash.push(_element.id)
+    decodedHash.push({[_layer.id]: _element.id})
+}
+
+const drawLayer = async (_layer, _edition) => {
+    // Trazi random element iz layera
+    let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)]
+    // Dodaje atribute za metadata od trenutnog elementa i layera kojemu pripada
+    addAttributes(element, _layer)
+    // Ucitava element kao sliku, sprema u context/canvas varijablu
+    const image = await loadImage(`${_layer.location}${element.fileName}`)
+    ctx.drawImage(image, _layer.position.x, _layer.position.y, _layer.size.width, _layer.size.height)
+    console.log(`EDITION ${_edition} ||  Created the ${_layer.name} layer, and choose element ${element.fileName}`)
+    
+    saveLayer(canvas, _edition)
+}
+
+for (let i = 1; i <= edition; i++) {
+     layers.forEach( layer => {
+       drawLayer(layer, i);
+    })
+    addMetaData(i)
+    console.log("Creating edition" + i)
+}
+
+fs.readFile("./output/_metadata.json", (err, data) => {
+    if(err) throw err
+    fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata))
+})
--- input/config.js
+++ input/config.js
@@ -47,8 +47,9 @@ const getElements = path => {
   };
 
 
-// Definiranje svih layera koji ce biti sastavljeni u canvas, dizajn bi trebao biti nacrtan ali ja nisam umjetnik pa sam skinuo ikonice s interneta
-// Pozicija x-y bi kod svih trebala biti 0,0, ali radi preglednosti 
+// Definiranje svih layera koji ce biti sastavljeni u canvas, dizajn bi trebao biti nacrtan u 1000x1000 formatu 
+// Nemam dizajn, zbog toga sam skinuo random ikonice i stavio ih da pocinju sa razlicitih mjesta, u njihovim originalnim velicinama
+// Zbog preglednosti funkcionalnosti
 const layers = [{
     id: 1,
     name: "background",
@@ -63,7 +64,7 @@ const layers = [{
     location: `${dir}/layer1/`,
     elements: getElements(`${dir}/layer1/`),
     position: { x: 100, y: 100},
-    size: {width: width, height: height}
+    size: {width: 100, height: 100}
 },
 {
     id: 3,
@@ -71,7 +72,7 @@ const layers = [{
     location: `${dir}/layer2/`,
     elements: getElements(`${dir}/layer2/`),
     position: { x: 200, y: 200},
-    size: {width: width, height: height}
+    size: {width: 100, height: 100}
 },
 {
     id: 4,
@@ -79,7 +80,7 @@ const layers = [{
     location: `${dir}/layer3/`,
     elements: getElements(`${dir}/layer3/`),
     position: { x: 300, y: 300},
-    size: {width: width, height: height}
+    size: {width: 100, height: 100}
 },
 {
     id: 5,
@@ -87,7 +88,31 @@ const layers = [{
     location: `${dir}/layer4/`,
     elements: getElements(`${dir}/layer4/`),
     position: { x: 400, y: 400},
-    size: {width: width, height: height}
+    size: {width: 100, height: 100}
+},
+{
+    id: 6,
+    name: "layer5",
+    location: `${dir}/layer5/`,
+    elements: getElements(`${dir}/layer5/`),
+    position: { x: 500, y: 500},
+    size: {width: 100, height: 100}
+},
+{
+    id: 7,
+    name: "layer6",
+    location: `${dir}/layer6/`,
+    elements: getElements(`${dir}/layer6/`),
+    position: { x: 600, y: 600},
+    size: {width: 100, height: 100}
+},
+{
+    id: 8,
+    name: "layer7",
+    location: `${dir}/layer7/`,
+    elements: getElements(`${dir}/layer7/`),
+    position: { x: 700, y: 700},
+    size: {width: 100, height: 100}
 },
 ]
 
